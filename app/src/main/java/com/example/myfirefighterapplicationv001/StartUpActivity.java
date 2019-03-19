2761e7394d4c769d1abbe53f7a24cb1e2eb64d08
zhexi.wu <zhexi.wu@student.kuleuven.be>
2019-03-19 12:07:42 +0100

    update on querying bluetooth device. It's not working yet

--- app/src/main/java/com/example/myfirefighterapplicationv001/StartUpActivity.java
+++ app/src/main/java/com/example/myfirefighterapplicationv001/StartUpActivity.java
@@ -1,29 +1,60 @@
 package com.example.myfirefighterapplicationv001;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class StartUpActivity extends AppCompatActivity {
     BluetoothAdapter adapter;
+    public ArrayList<BluetoothDevice> mBTDevices = new ArrayList<>();
+    public DeviceListAdapter mDeviceListAdapter;
+    ListView lvNewDevices;
+    private static final String TAG = "StartUpActivity";
+
+    private BroadcastReceiver mBroadcastReceiver3 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            Log.d(TAG, "onReceive: ACTION FOUND.");
+
+            if (action.equals(BluetoothDevice.ACTION_FOUND)){
+                BluetoothDevice device = intent.getParcelableExtra (BluetoothDevice.EXTRA_DEVICE);
+                mBTDevices.add(device);
+                Log.d(TAG, "onReceive: " + device.getName() + ": " + device.getAddress());
+                mDeviceListAdapter = new DeviceListAdapter(context, R.layout.device_adapter_view, mBTDevices);
+                lvNewDevices.setAdapter(mDeviceListAdapter);
+            }
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         adapter = BluetoothAdapter.getDefaultAdapter();
-
         setContentView(R.layout.activity_start_up);
         Button conBtn = findViewById(R.id.conBtn);
         Button bltBtn = findViewById(R.id.bltBtn);
         Button testBtn = findViewById(R.id.testBtn);
 
+        lvNewDevices = (ListView) findViewById(R.id.lvDevices);
+        mBTDevices = new ArrayList<>();
         //test for viewing paired devices(works now!)
         testBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,5 +110,44 @@ public class StartUpActivity extends AppCompatActivity {
             }
         }
     }
-}
+
+    public void btnDiscover(View view) {
+        Log.d(TAG,"looking for devices");
+        if(adapter.isDiscovering()){
+            adapter.cancelDiscovery();
+            Log.d(TAG, "btnDiscover: Canceling discovery.");
+
+            //check BT permissions in manifest
+            checkBTPermissions();
+
+            adapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mBroadcastReceiver3, discoverDevicesIntent);
+        }
+        if(!adapter.isDiscovering()){
+
+            //check BT permissions in manifest
+            checkBTPermissions();
+
+            adapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mBroadcastReceiver3, discoverDevicesIntent);
+        }
+
+    }
+
+    private void checkBTPermissions(){
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
+    }
+
 /*git enabled*/
